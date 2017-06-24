a31d72204985a5f268d04d1d090d64598c115476
Saran Ahluwalia <ahlusar.ahluwalia@gmail.com>
2017-06-24 18:44:06 -0400

    finished board and server - multithreading

--- src/minesweeper/Board.java
+++ src/minesweeper/Board.java
@@ -1,26 +1,23 @@
 
 package minesweeper;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
-
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
- *
+ * TODO: Specification
  */
 public class Board {
-
-
-    // Rep invariant:
-    //   board as a grid of squares. Each square is either flagged , dug , or untouched .
-    //   Each square also either contains a bomb, or does not contain a bomb.
-    // Abstraction Function:
-    //   represents a board for Minesweeper
-    // Safety from rep exposure:
-    //   All fields are private and elements of fields are immutable,
-    //   methods will return a defensive copy.
-
 
     private static double BOMB_PROBABILITY = .25;
     private static String BOMB_SYMBOL = "1";
@@ -45,30 +42,201 @@ public class Board {
         buildBoardUsingSizeAndBombList();
     }
 
-    private void buildBombsFromList(List<Point> bomblist){
+    // ----------------- build bomb helpers --------------
 
+    public static Board buildRandomBoard(int sizeX, int sizeY) {
+        return new Board(sizeX, sizeY, buildRandomBombList(sizeX, sizeY));
     }
 
-    private void buildBoardFromFile(String inputFile){
-
+    private static List<Point> buildRandomBombList(int sizeX, int sizeY) {
+        Random random = new Random(0);
+        List<Point> bombList = new ArrayList<>();
+        for (int y = 0; y < sizeY; y++)  {
+            for (int x = 0; x < sizeX; x++){
+                double probability = random.nextFloat();
+                if (probability <= BOMB_PROBABILITY) {
+                    bombList.add(new Point(x, y));
+                }
+            }
+        }
+        return bombList;
     }
 
-
-    private void buildBoardUsingSizeAndBombList(){
-
+    private void buildBombsFromList(List<Point> bombsList) {
+        bombs = new ConcurrentHashMap<>();
+        bombsList.forEach(point -> bombs.put(point, true));
     }
 
-    private void getBombsFromLine(String line, int y){
-
+    private void buildBoardUsingSizeAndBombList() {
+        board = new ConcurrentHashMap<>();
+        for (int x = 0; x < boardXSize; x++) {
+            for (int y = 0; y < boardYSize; y++) {
+                Point point = new Point(x, y);
+                board.computeIfAbsent(point, p -> new Square(p, bombs.containsKey(p)));
+            }
+        }
     }
 
-    public int getBoardXSize(){
+    // ----------------- build bomb from file --------------
+
+    private void buildBoardFromFile(String fileName) {
+        try(Stream<String> linesStream = Files.lines(Paths.get(fileName))) {
+            List<String> linesList = linesStream.collect(Collectors.toList());
+            getBoardSizeFromLine(linesList.get(0));
+            buildBombsFromFile(linesList);
+            buildBoardUsingSizeAndBombList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void buildBombsFromFile(List<String> linesList) {
+        List<Point> bombsList = Stream.iterate(0, y -> y + 1)
+                .limit(linesList.size() - 1)
+                .flatMap(y -> getBombsFromLine(linesList.get(y + 1), y))
+                .collect(Collectors.toList());
+        buildBombsFromList(bombsList);
+    }
+
+    private void getBoardSizeFromLine(String line) throws IOException {
+        String[] size = Pattern.compile("\\s{1}").split(line);
+        boardXSize = Integer.parseInt(size[0]);
+        boardYSize = Integer.parseInt(size[1]);
+    }
+
+    private Stream<Point> getBombsFromLine(String line, int y) {
+        String[] lineArr = Pattern.compile("\\s{1}").split(line);
+        return IntStream.range(0, lineArr.length)
+                .filter(x -> lineArr[x].equals(BOMB_SYMBOL))
+                .mapToObj(x -> new Point(x, y));
+    }
+
+    // -------------- getters ----------------------------
+    public int getBoardXSize() {
         return boardXSize;
     }
 
-    public int getBoardYSize(){
+    public int getBoardYSize() {
         return boardYSize;
     }
+
+    public ConcurrentHashMap<Point, Square> getBoard() {
+        return board;
+    }
+
+    public ConcurrentHashMap<Point, Boolean> getBombs() {
+        return bombs;
+    }
+
+    // -------------- operations on the board ------------
+
+    public void setSquareDug(Point point) {
+        board.computeIfPresent(point, (p, s) -> s.setDug());
+    }
+
+    public void setSquareFlagged(Point point, boolean isFlagged) {
+        board.computeIfPresent(point, (p, s) -> s.setFlagged(isFlagged));
+    }
+
+    public void removeBomb(Point point) {
+        board.computeIfPresent(point, (p, s) -> s.removeBomb());
+        bombs.remove(point);
+    }
+
+    // ------------- calculate number of bombs -----------
+
+    public int getBombCount(Point point) {
+        Square square = board.get(point);
+        return getAdjSquares(point)
+                .stream()
+                .mapToInt(s -> s.getBombCount())
+                .sum();
+    }
+
+    public List<Point> getAdjPoints(Point point) {
+        List<Point> adjPoints = new CopyOnWriteArrayList<>();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                Point p = new Point(point.getX() + i, point.getY() + j);
+                if (!p.equals(point) && isOnBoard(p)) {
+                    adjPoints.add(p);
+                }
+            }
+        }
+        return adjPoints;
+    }
+
+    public List<Square> getAdjSquares(Point point) {
+        List<Point> adjPoints = getAdjPoints(point);
+        return adjPoints.stream().map(p -> board.get(p)).collect(Collectors.toList());
+    }
+
+    public boolean isOnBoard(Point point) {
+        return 0 <= point.getX() && point.getX() < boardXSize &&
+                0 <= point.getY() && point.getY() < boardYSize;
+    }
+
+    // -------------- printing --------------------------
+
+    @Override
+    public String toString() {
+        StringBuffer boardStrBuf = new StringBuffer();
+        for (int y = 0; y < boardYSize; y++)  {
+            for (int x = 0; x < boardXSize; x++) {
+                Point point = new Point(x, y);
+                Square square = board.get(point);
+                int bombCount = getBombCount(point);
+                if (square.isDug() && bombCount > 0) {
+                    boardStrBuf.append(Integer.toString(bombCount));
+                } else {
+                    boardStrBuf.append(board.get(point).toString());
+                }
+                if (x != boardXSize - 1) {
+                    boardStrBuf.append(" ");
+                }
+            }
+            if (y < boardYSize - 1) {
+                boardStrBuf.append("\n");
+            }
+        }
+        return boardStrBuf.toString();
+    }
+
+    // debugging printing for board
+    public String printBoard() {
+        StringBuffer boardStrBuf = new StringBuffer();
+        for (int y = 0; y < boardYSize; y++)  {
+            for (int x = 0; x < boardXSize; x++){
+                Point point = new Point(x, y);
+                if (board.get(point).isContainBomb()) {
+                    boardStrBuf.append("B");
+                } else {
+                    boardStrBuf.append(board.get(point).toString());
+                }
+            }
+            boardStrBuf.append("\n");
+        }
+        return boardStrBuf.toString();
+    }
+
+    public String printBoardWithBombCount() {
+        StringBuffer boardStrBuf = new StringBuffer();
+        for (int x = 0; x < boardXSize; x++) {
+            for (int y = 0; y < boardYSize; y++) {
+                Point point = new Point(y, x);
+                if (board.get(point).isContainBomb()) {
+                    boardStrBuf.append("B");
+                } else if (getBombCount(point) > 0) {
+                    boardStrBuf.append(getBombCount(point));
+                } else {
+                    boardStrBuf.append(board.get(point).toString());
+                }
+            }
+            boardStrBuf.append("\n");
+        }
+        return boardStrBuf.toString();
+    }
+
     // -------------- main --------------------------
 
     public static void main(String[] args) {
@@ -76,7 +244,7 @@ public class Board {
         String line = "0 1 0 1 0 1";
         int y = 5;
         Board b = new Board();
-        b.getBombsFromLine(line, y);
+        b.getBombsFromLine(line, y).forEach(System.out::println);
 
     }
 }
--- src/minesweeper/server/MinesweeperServer.java
+++ src/minesweeper/server/MinesweeperServer.java
@@ -1,9 +1,8 @@
-/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
 package minesweeper.server;
 
 import minesweeper.Board;
+import minesweeper.Point;
+import minesweeper.Square;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -16,6 +15,13 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class MinesweeperServer {
 
+    private static final int DEFAULT_PORT = 4444;
+    private static final int MAXIMUM_PORT = 65535;
+    public static final int DEFAULT_SIZE = 10;
+
+    private final ServerSocket serverSocket;
+    private final boolean debug;
+
     private static String BOOM_MESSAGE = "BOOM!";
     private static String HELP_MESSAGE = "This is the help message!";
     private static String TERMINATE = "terminate";
@@ -25,169 +31,34 @@ public class MinesweeperServer {
     private String helloMessage;
 
 
-    // System thread safety argument
-    //   TODO  5
-
-    /** Default server port. */
-    private static final int DEFAULT_PORT = 4444;
-    /** Maximum port number as defined by ServerSocket. */
-    private static final int MAXIMUM_PORT = 65535;
-    /** Default square board size. */
-    private static final int DEFAULT_SIZE = 10;
-
-    /** Socket for receiving incoming connections. */
-    private final ServerSocket serverSocket;
-    /** True if the server should *not* disconnect a client after a BOOM message. */
-    private final boolean debug;
-
-    // TODO: Abstraction function, rep invariant, rep exposure
-
-    /**
-     * Make a MinesweeperServer that listens for connections on port.
-     *
-     * @param port port number, requires 0 <= port <= 65535
-     * @param debug debug mode flag
-     * @throws IOException if an error occurs opening the server socket
-     */
     public MinesweeperServer(int port, boolean debug) throws IOException {
         serverSocket = new ServerSocket(port);
         this.debug = debug;
+
     }
 
-    /**
-     * Run the server, listening for client connections and handling them.
-     * Never returns unless an exception is thrown.
-     *
-     * @throws IOException if the main server socket is broken
-     *                     (IOExceptions from individual clients do *not* terminate serve())
-     */
     public void serve() throws IOException {
         while (true) {
-            // block until a client connects
             Socket socket = serverSocket.accept();
-
-            // handle the client
-            try {
-                handleConnection(socket);
-            } catch (IOException ioe) {
-                ioe.printStackTrace(); // but don't terminate serve()
-            } finally {
-                socket.close();
-            }
+            playersNum.incrementAndGet();
+            buildHelloMessage();
+            Runnable r = new ConnectionHadler(socket);
+            Thread t = new Thread(r);
+            t.start();
         }
     }
 
-    /**
-     * Handle a single client connection. Returns when client disconnects.
-     *
-     * @param socket socket where the client is connected
-     * @throws IOException if the connection encounters an error or terminates unexpectedly
-     */
-    private void handleConnection(Socket socket) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-        try {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                String output = handleRequest(line);
-                if (output != null) {
-                    // TODO: Consider improving spec of handleRequest to avoid use of null
-                    out.println(output);
-                }
-            }
-        } finally {
-            out.close();
-            in.close();
-        }
+    private void setBoard(Board board) {
+        this.board = board;
     }
 
-    /**
-     * Handler for client input, performing requested operations and returning an output message.
-     *
-     * @param input message from client
-     * @return message to client, or null if none
-     */
-    private String handleRequest(String input) {
-        String regex = "(look)|(help)|(bye)|"
-                     + "(dig -?\\d+ -?\\d+)|(flag -?\\d+ -?\\d+)|(deflag -?\\d+ -?\\d+)";
-        if ( ! input.matches(regex)) {
-            // invalid input
-            // TODO Problem 5
-        }
-        String[] tokens = input.split(" ");
-        if (tokens[0].equals("look")) {
-            // 'look' request
-            // TODO  5
-        } else if (tokens[0].equals("help")) {
-            // 'help' request
-            // TODO  5
-        } else if (tokens[0].equals("bye")) {
-            // 'bye' request
-            // TODO  5
-        } else {
-            int x = Integer.parseInt(tokens[1]);
-            int y = Integer.parseInt(tokens[2]);
-            if (tokens[0].equals("dig")) {
-                // 'dig x y' request
-                // TODO  5
-            } else if (tokens[0].equals("flag")) {
-                // 'flag x y' request
-                // TODO  5
-            } else if (tokens[0].equals("deflag")) {
-                // 'deflag x y' request
-                // TODO  5
-            }
-        }
-        // TODO: Should never get here, make sure to return in each of the cases above
-        throw new UnsupportedOperationException();
+    private void buildHelloMessage() {
+        helloMessage = String.format("Welcome to Minesweeper. Players: %d including you. "
+                        + "Board: %d columns by %d rows. Type 'help' for help.", playersNum.intValue(),
+                board.getBoardXSize(), board.getBoardYSize());
     }
 
-    /**
-     * Start a MinesweeperServer using the given arguments.
-     *
-     * <br> Usage:
-     *      MinesweeperServer [--debug | --no-debug] [--port PORT] [--size SIZE_X,SIZE_Y | --file FILE]
-     *
-     * <br> The --debug argument means the server should run in debug mode. The server should disconnect a
-     *      client after a BOOM message if and only if the --debug flag was NOT given.
-     *      Using --no-debug is the same as using no flag at all.
-     * <br> E.g. "MinesweeperServer --debug" starts the server in debug mode.
-     *
-     * <br> PORT is an optional integer in the range 0 to 65535 inclusive, specifying the port the server
-     *      should be listening on for incoming connections.
-     * <br> E.g. "MinesweeperServer --port 1234" starts the server listening on port 1234.
-     *
-     * <br> SIZE_X and SIZE_Y are optional positive integer arguments, specifying that a random board of size
-     *      SIZE_X*SIZE_Y should be generated.
-     * <br> E.g. "MinesweeperServer --size 42,58" starts the server initialized with a random board of size
-     *      42*58.
-     *
-     * <br> FILE is an optional argument specifying a file pathname where a board has been stored. If this
-     *      argument is given, the stored board should be loaded as the starting board.
-     * <br> E.g. "MinesweeperServer --file boardfile.txt" starts the server initialized with the board stored
-     *      in boardfile.txt.
-     *
-     * <br> The board file format, for use with the "--file" option, is specified by the following grammar:
-     * <pre>
-     *   FILE ::= BOARD LINE+
-     *   BOARD ::= X SPACE Y NEWLINE
-     *   LINE ::= (VAL SPACE)* VAL NEWLINE
-     *   VAL ::= 0 | 1
-     *   X ::= INT
-     *   Y ::= INT
-     *   SPACE ::= " "
-     *   NEWLINE ::= "\n" | "\r" "\n"?
-     *   INT ::= [0-9]+
-     * </pre>
-     *
-     * <br> If neither --file nor --size is given, generate a random board of size 10x10.
-     *
-     * <br> Note that --file and --size may not be specified simultaneously.
-     *
-     * @param args arguments as described
-     */
     public static void main(String[] args) {
-        // Command-line argument parsing is provided. Do not change this method.
         boolean debug = false;
         int port = DEFAULT_PORT;
         int sizeX = DEFAULT_SIZE;
@@ -242,25 +113,147 @@ public class MinesweeperServer {
         }
     }
 
-    /**
-     * Start a MinesweeperServer running on the specified port, with either a random new board or a
-     * board loaded from a file.
-     *
-     * @param debug The server will disconnect a client after a BOOM message if and only if debug is false.
-     * @param file If file.isPresent(), start with a board loaded from the specified file,
-     *             according to the input file format defined in the documentation for main(..).
-     * @param sizeX If (!file.isPresent()), start with a random board with width sizeX
-     *              (and require sizeX > 0).
-     * @param sizeY If (!file.isPresent()), start with a random board with height sizeY
-     *              (and require sizeY > 0).
-     * @param port The network port on which the server should listen, requires 0 <= port <= 65535.
-     * @throws IOException if a network error occurs
-     */
-    public static void runMinesweeperServer(boolean debug, Optional<File> file, int sizeX, int sizeY, int port) throws IOException {
-
-        // TODO: Continue implementation 4
-
+    public static void runMinesweeperServer(boolean debug, Optional<File> file,
+                                            int sizeX, int sizeY, int port) throws IOException {
         MinesweeperServer server = new MinesweeperServer(port, debug);
+        if (!file.isPresent()) {
+            server.setBoard(Board.buildRandomBoard(sizeX, sizeY));
+        } else {
+            Board board = new Board(file.get().toString());
+            server.setBoard(board);
+        }
         server.serve();
     }
+
+    class ConnectionHadler implements Runnable {
+
+        private Socket socket;
+
+        public ConnectionHadler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                handleConnection(socket);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        private void handleConnection(Socket socket) throws IOException {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            try {
+                out.println(helloMessage);
+                for (String line = in.readLine(); line != null; line = in.readLine()) {
+                    String output = handleRequest(line);
+                    if (output != null) {
+                        // TODO: Consider improving spec of handleRequest to avoid use of null
+                        if (output.equals(TERMINATE)) {
+                            playersNum.decrementAndGet();
+                            break;
+                        } else if (output.equals(BOOM_MESSAGE) && !debug) {
+                            out.println(output);
+                            playersNum.decrementAndGet();
+                            break;
+                        } else {
+                            out.println(output);
+                        }
+                    }
+                }
+            } finally {
+                out.close();
+                in.close();
+            }
+        }
+
+        private String handleRequest(String input) {
+            String regex = "(look)|(help)|(bye)|"
+                    + "(dig -?\\d+ -?\\d+)|(flag -?\\d+ -?\\d+)|(deflag -?\\d+ -?\\d+)";
+            if ( ! input.matches(regex)) {
+                // invalid input
+                return HELP_MESSAGE;
+            }
+            String[] tokens = input.split(" ");
+            if (tokens[0].equals("look")) {
+                // 'look' request
+                return board.toString();
+            } else if (tokens[0].equals("help")) {
+                // 'help' request
+                return HELP_MESSAGE;
+            } else if (tokens[0].equals("bye")) {
+                // 'bye' request
+                return TERMINATE;
+            } else {
+                int x = Integer.parseInt(tokens[1]);
+                int y = Integer.parseInt(tokens[2]);
+                Point point = new Point(x, y);
+                Square square = board.getBoard().get(point);
+                if (tokens[0].equals("dig")) {
+                    // 'dig x y' request
+                    return handleDig(x, y);
+                } else if (tokens[0].equals("flag")) {
+                    // 'flag x y' request
+                    if (board.isOnBoard(point) && square.isUntouched()) {
+                        square.setFlagged(true);
+                        return board.toString();
+                    }
+                } else if (tokens[0].equals("deflag")) {
+                    // 'deflag x y' request
+                    if (board.isOnBoard(point) && square.isUntouched()) {
+                        square.setFlagged(false);
+                        return board.toString();
+                    }
+                }
+            }
+            throw new UnsupportedOperationException();
+        }
+
+        private String handleDig(int x, int y) {
+            Point point = new Point(x, y);
+            Square square = board.getBoard().get(point);
+            if (!board.isOnBoard(point) || !square.isUntouched()) {
+                return board.toString();
+            } else if (square.isUntouched()) {
+                square.setDug();
+                if (square.isContainBomb()) {
+                    board.removeBomb(point);
+                    handleDigNeighbors(point);
+                    return BOOM_MESSAGE;
+                } else {
+                    handleDigNeighbors(point);
+                    return board.toString();
+                }
+            } else {
+                return board.toString();
+            }
+        }
+
+        private void handleDigNeighbors(Point point) {
+            if (board.getBombCount(point) == 0) {
+                board.getAdjPoints(point)
+                        .stream()
+                        .forEach(p -> {
+                            Square square = board.getBoard().get(p);
+                            if (square.isUntouched()) {
+                                square.setDug();
+                                handleDigNeighbors(p);
+                            }
+                        });
+            }
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
